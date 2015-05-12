ac8c0d486fef593db8b9cee6953bc938c38e5e43
LeoSENJU <yysenju@gmail.com>
2015-05-12 23:20:12 +0800

    Change the break it view controller

---
+++ BreakIt/BreakIt/BreakItViewController.swift
@@ -0,0 +1,12 @@
+//
+//  BreakItViewController.swift
+//  BreakIt
+//
+//  Created by yangyang on 5/12/15.
+//  Copyright (c) 2015 yangyang. All rights reserved.
+//
+
+import UIKit
+
+class BreakItViewController: UIViewController {
+}
