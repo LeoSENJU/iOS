9335592b49b46f579880f022c13d9214a33426f8
LeoSENJU <yysenju@gmail.com>
2015-05-13 22:56:35 +0800

    replace the original single view controller with tab view controller

---
+++ BreakIt/BreakIt/MoreViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MoreViewController.swift
+//  BreakIt
+//  View controller for future setting
+//  Created by yangyang on 5/13/15.
+//  Copyright (c) 2015 yangyang. All rights reserved.
+//
+
+import UIKit
+
+class MoreViewController: UIViewController {
+
+}
---
+++ BreakIt/BreakIt/SettingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.swift
+//  BreakIt
+//  View controller for setting different parameter of BreakIt game
+//  Created by yangyang on 5/13/15.
+//  Copyright (c) 2015 yangyang. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+}
