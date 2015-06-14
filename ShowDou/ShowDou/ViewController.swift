11dd57376091740201b3831beca8c09d3b110f83
LeoSENJU <yysenju@gmail.com>
2015-06-14 15:41:36 +0800

    new project of using the DOUBAN API

---
+++ ShowDou/ShowDou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowDou
+//
+//  Created by yangyang on 6/14/15.
+//  Copyright (c) 2015 yangyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
